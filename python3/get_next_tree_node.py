abe0c80d3bd5733a9cbf667431eab86229670628
baixiang <35680441+baixianghuang@users.noreply.github.com>
2019-06-02 17:31:16 +0800

    Create get_next_tree_node.py

---
+++ python3/get_next_tree_node.py
@@ -0,0 +1,56 @@
+class BinaryTreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.father = None
+
+
+def get_next(node):
+    """get the next node of a binary tree's inorder traverse"""
+    if node == None: 
+        return False
+    
+    # if the input node is a left leaf node
+    if not node.left and not node.right and node.father.left == node:
+        return node.father
+    
+    # if the input node is a root that has right sub-tree
+    if node.right:
+        node = node.right
+        while node.left:
+            node = node.left
+        return node
+
+    # f the input node is a right leaf node
+    if not node.left and not node.right and node.father.right == node:
+        while node.father:
+            if node.father.left == node:
+                return node.father
+            node = node.father
+        return None
+
+
+root = BinaryTreeNode('a')
+node_b = BinaryTreeNode('b')
+node_c = BinaryTreeNode('c')
+node_d = BinaryTreeNode('d')
+node_e = BinaryTreeNode('e')
+node_h = BinaryTreeNode('h')
+node_i = BinaryTreeNode('i')
+root.left = node_b
+node_b.father = root
+root.right = node_c
+node_c.father = root
+node_b.left = node_d
+node_d.father = node_b
+node_b.right = node_e
+node_e.father = node_b
+node_e.left = node_h
+node_h.father = node_e
+node_e.right = node_i
+node_i.father = node_e
+
+print(get_next(node_h).val)
+print(get_next(node_b).val)
+print(get_next(node_i).val)
