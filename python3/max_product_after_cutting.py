8a6a7adbd1f1c10bdce9c4537e8b178eed2dadd6
baixiang <35680441+baixianghuang@users.noreply.github.com>
2019-07-11 17:45:08 +0800

    Update max_product_after_cutting.py

--- python3/max_product_after_cutting.py
+++ python3/max_product_after_cutting.py
@@ -1,26 +1,18 @@
 def max_product_after_cutting(length):
-    ""a rope with length n is divided into m (integer) parts"""
+    """a rope is divided into m (integer) parts"""
     if length <= 0:
         return None
     elif 0 < length < 2:
         return 0
-    elif length == 2:  # max_product_list[2] != 1
+    elif length == 2:
         return 1
-    elif length == 3:  # max_product_list[3] != 2
+    elif length == 3:
         return 2
 
-    max_product_list = [0, 1, 2, 3]
-
+    ls = [0, 1, 2, 3]  # store max product with different length
     for i in range(4, length+1):
-        max_p = 0
-        for j in range(1, i//2+1):
-            if max_p < max_product_list[j] * max_product_list[i-j]:
-                max_p = max_product_list[j] * max_product_list[i-j]
-        max_product_list.append(max_p)
-        # print(max_p)
-
-    print(max_product_list[length])
-    return max_product_list[length]
+        ls.append(max(ls[i-2]*2, ls[i-3]*3))
+    return ls[length]
 
 
-max_product_after_cutting(9)
+print(max_product_after_cutting(9))
