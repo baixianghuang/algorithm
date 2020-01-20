da5a2446b6c3fb0c882b3e01b25cd5a235f033e9
baixiang <35680441+baixianghuang@users.noreply.github.com>
2020-01-20 11:12:47 +0800

    Create ReverseList.java

---
+++ Java/ReverseList.java
@@ -0,0 +1,34 @@
+class ReverseList {
+//    public class ListNode {
+//        int val;
+//        ListNode next;
+//        ListNode(int x) { val = x; }
+//    }
+
+    public ListNode reverseList(ListNode head) {
+        // Time complexity: O(n)
+        if (head == null)
+            return null;
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode nex = head.next;
+        cur.next = null;
+        while (nex != null) {
+            prev = cur;
+            cur = nex;
+            nex = nex.next;
+            cur.next = prev;
+        }
+        return cur;
+    }
+
+    public ListNode reverseListRecursively(ListNode node) {
+        // Time complexity: O(n)
+        if (node == null || node.next == null)
+            return null;
+        ListNode n = reverseListRecursively(node.next);
+        node.next.next = node;
+        node.next = null;
+        return n;
+    }
+}
