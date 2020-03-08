3c53b05b3f1b5b7ecda4df46b6e88f42f079346d
baixiang <35680441+baixianghuang@users.noreply.github.com>
2020-03-08 09:52:50 +0800

    Update NumSquares.java

--- Java/NumSquares.java
+++ Java/NumSquares.java
@@ -1,26 +1,50 @@
 NumSquares {
-    public int numSquares(int n) {
-        // BFS
-        if(n <= 0) { return 0; }
-        LinkedList<Pair<Integer, Integer>> queue = new LinkedList<>();
-        queue.addLast(new Pair<>(n, 0));
+    public int numSquaresBFS(int n) {
+        if (n <= 0) { return 0; }
+        Queue<Integer> queue = new LinkedList<>();
         boolean[] visited = new boolean[n+1];
         visited[n] = true;
-
-        while(!queue.isEmpty()){
-            Pair<Integer, Integer> cur = queue.poll();
-            int val = cur.getKey();
-            int step = cur.getValue();
-//            if (val == 0) { return step; }  // move this step to the for loop to enhance performance
-            for (int i = 1; val - i*i >= 0 ; i++) {
+        queue.offer(n);
+        int step = 0;
+        while (!queue.isEmpty()) {
+            int layerSize = queue.size();
+            for (int i = 0; i < layerSize; i++) {
+                int val = queue.poll();
+                for (int j = 1; val - j*j >= 0; j++) {
+                    int a = val - j*j;
+                    if (a == 0) { return step+1; }
+                    if (!visited[a]) {
+                        queue.offer(a);
+                        visited[a] = true;
+                    }
+                }
+            }
+            step++;
+        }
+        throw new IllegalArgumentException("No solution due to invalid input");
+    }
+    
+    public int numSquaresBFSApproach2(int n) {
+        //using Pair to store the number and the step it takes to reach this number
+        if (n <= 0) { return 0; }
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n+1];
+        visited[n] = true;
+        queue.offer(new Pair<>(n, 0));
+        while (!queue.isEmpty()) {
+            Pair<Integer, Integer> pair = queue.poll();
+            int val = pair.getKey();
+            int step = pair.getValue();
+            System.out.println(pair);
+            for (int i = 1; val - i*i >= 0; i++) {
                 int a = val - i*i;
-                if (a == 0) { return step + 1; }
+                if (a == 0) { return step+1; }
                 if (!visited[a]) {
-                    queue.offer(new Pair<>(a,step+1));
+                    queue.offer(new Pair<>(a, step+1));
                     visited[a] = true;
                 }
             }
         }
-        throw new IllegalStateException("No Solution.");
+        throw new IllegalArgumentException("No solution due to invalid input");
     }
 }
