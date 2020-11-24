7aebe8c614050c9b51e010069bd3865c0591ba8a
Huang <35680441+baixianghuang@users.noreply.github.com>
2020-11-24 16:52:00 +0800

    Update Intersection.java

--- Java/Intersection.java
+++ Java/Intersection.java
@@ -1,5 +1,25 @@
 class Intersection {
     public int[] intersection(int[] nums1, int[] nums2) {
+        // O(1) space, O(nlogn) time
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        Set<Integer> ans = new HashSet<>();
+        for (int i = 0, j = 0; i < nums1.length && j < nums2.length; ) {
+            if (nums1[i] == nums2[j]) {
+                ans.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return ans.stream().mapToInt(Integer::intValue).toArray();
+    }
+    
+    public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> hs1 = new HashSet<>();
         Set<Integer> hs2 = new HashSet<>();
         for (int n : nums1) {
